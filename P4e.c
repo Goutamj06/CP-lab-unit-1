177b827056631d2381dc0c437c0398aad17fe47c
Goutamj06 <goutamj408@gmail.com>
2025-11-19 00:44:54 +0530

    Implement calculator program using switch case
    Replaced multiplication table program with a calculator using switch case.

--- P4e.c
+++ P4e.c
@@ -1,20 +1,30 @@
-C Program to print multiplication Table
-
-
-
-#include <stdio.h>
-
-int main() {
-    int num, i;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    printf("\nMultiplication Table of %d\n", num);
-
-    for(i = 1; i <= 10; i++) {
-        printf("%d x %d = %d\n", num, i, num * i);
+//(4e) c program calulator using switch case
+#include<stdio.h>
+int main()
+{
+    int choice;
+    printf("enter your choice b/w 1-3");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+        printf("user entered choice 1");
+        break;
+        case 2:
+        printf("user entered choice 2");
+        break;
+        case 3:
+        printf("user entered choice 3");
+        break;
+        default:
+        printf("invalid choice");
+        
     }
-
+    
     return 0;
 }
+
+output
+
+enter your choice b/w 1-3:2
+user entered choice 2
