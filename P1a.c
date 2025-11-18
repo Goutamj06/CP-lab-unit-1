b1d36ebb534bcd14b5d611b7b1cc3c2169ad01d5
Goutamj06 <goutamj408@gmail.com>
2025-11-19 00:20:25 +0530

    Modify program to calculate circle area
    Updated the program to calculate the area of a circle based on user input.

--- P1a.c
+++ P1a.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
+//(1a)c program for receiving an input from the user at the runtime and printing it
 
-int main() {
-    int num;
-    printf("Enter a number: ");    
-    scanf("%d", &num); 
-
-    printf("You entered: %d\n", num);   
-
-    return 0;
+#include<stdio.h>
+int main()
+{
+    float pi= 3.14;
+    float  area,r;
+    printf("enter the radius of the circle=");
+    scanf("%f",&r);
+    area= pi*r*r;
+    printf("area of the circle is=%f\n",area);
+    return 0;
+    
 }
+
+output
+enter the radius of the circle=4
+area of the circle is=50.240002
